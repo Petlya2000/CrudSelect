26c865ebfcca3c3e50fc9f5f6a31e0416516ca25
Petlya2000 <135063891+Petlya2000@users.noreply.github.com>
2023-10-21 17:24:13 +0300

    Update rec.php

--- crud+select/rec.php
+++ crud+select/rec.php
@@ -1,12 +1,12 @@
 <?php 
-require 'connect.php'; // Подключает файл с логином/паролем и именем БД
-mysql_set_charset('utf8'); // Устанавливает кодировку клиента
+require_once 'connect.php'; // Подключает файл с логином/паролем и именем БД
+$id = trim($_REQUEST['id']);
 $author = trim($_REQUEST['author']); // Получает содержимое поля "Автор" и убирает возможные пробелы в начале строки
 $title = trim($_REQUEST['title']); // То же самое для поля "Название"
-$text = mysql_real_escape_string(trim($_REQUEST['text'])); // То же самое для поля "Текст" + (см.ниже)    
-$insert_sql = "INSERT INTO polka1 (author, title, text)" . // Указывает в какую таблицу и в какие поля ...
-                   "VALUES('{$author}', '{$title}', '{$text}');"; // ...записывать данные
-mysql_query($insert_sql); // отправляем данные в базу
+$text = mysqli_real_escape_string($conn,trim($_REQUEST['text'])); // То же самое для поля "Текст" + (см.ниже)    
+$insert_sql = "INSERT INTO polka1 (id,author, title, text)" . // Указывает в какую таблицу и в какие поля ...
+                   "VALUES('{$id}','{$author}', '{$title}', '{$text}');"; // ...записывать данные
+mysqli_query($conn,$insert_sql); // отправляем данные в базу
 ?>
 <html>
 <head>
