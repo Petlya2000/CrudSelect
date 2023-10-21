368436745542ad51fde92f2b0595238766ed3e5e
Petlya2000 <135063891+Petlya2000@users.noreply.github.com>
2023-10-21 16:59:40 +0300

    Update allauthor.php

--- crud+select/allauthor.php
+++ crud+select/allauthor.php
@@ -8,24 +8,25 @@
 </head>
 <body>
 <?php
-require 'connect.php'; // Подключает файл с логином/паролем и именем БД
-mysql_set_charset('utf8'); // Устанавливает кодировку клиента
+require_once 'connect.php'; // Подключает файл с логином/паролем и именем БД
 $sql_select = "SELECT * FROM polka1"; // Выбираем таблицу из которой читать данные
-$result = mysql_query($sql_select); // Запрос к БД
-$row = mysql_fetch_array($result); // Разбираем полученый массив 
+$result = mysqli_query($conn,$sql_select); // Запрос к БД
+$row = mysqli_fetch_array($result); // Разбираем полученый массив 
 do
 {
   printf("<p>Номер книги: ".$row['id']."</p><p>Автор: ".$row['author']."</p><p>Название: ".$row['title']
   ."</p>----------------------------------------<b>");
 }
-while($row = mysql_fetch_array($result));
+while($row = mysqli_fetch_array($result));
 ?>
 <form method='post' action='read.php'><b>
 <input id="Nknig" type='text' name='nk' placeholder="Номер книги"><b><b>
+<br>
 <input id='submitread'  type='submit' value='Читать...'><b><b>
 </form>
 <form method="post" action="index.html">
-<input id="submitback" type="submit" value="На главную">
+<br>
+  <input id="submitback" type="submit" value="На главную">
 </form>
 </body>
 </html>
