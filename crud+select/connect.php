32855382f4398c430eb8602841a3b0588f6aefa7
Petlya2000 <135063891+Petlya2000@users.noreply.github.com>
2023-10-21 17:01:28 +0300

    Update connect.php

--- crud+select/connect.php
+++ crud+select/connect.php
@@ -1,4 +1,6 @@
 <?php
-mysql_connect("localhost", "crud_user", "q1w2e3"); // логин, пароль
-mysql_select_db("homelib"); // имя бызы
+  $conn=mysqli_connect("localhost", "crud_user", "q1w2e3","homelib"); // логин, пароль
+    if (mysqli_connect_errno(){
+        echo "Failed to connect DB : " . mysqli_connect_error();
+}
 ?>
