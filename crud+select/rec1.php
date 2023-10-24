cbe35a52d1139744101dd2fabe3ea4adb88cf42b
Petlya2000 <135063891+Petlya2000@users.noreply.github.com>
2023-10-24 20:58:05 +0300

    Update rec1.php

--- crud+select/rec1.php
+++ crud+select/rec1.php
@@ -4,6 +4,13 @@ $id = trim($_REQUEST['id']);
 $author = trim($_REQUEST['author']); // Получает содержимое поля "Автор" и убирает возможные пробелы в начале строки
 $title = trim($_REQUEST['title']); // То же самое для поля "Название"
 $text = mysqli_real_escape_string($conn,trim($_REQUEST['text'])); // То же самое для поля "Текст" + (см.ниже)    
-$update_sql = "UPDATE polka1 SET name = '$author', age = '$title', text = '$text' WHERE id = '$id'
+$update_sql = "UPDATE polka1 SET author = '$author', title = '$title', text = '$text' WHERE id = '$id'
 mysqli_query($conn,$update_sql); // отправляем данные в базу
 ?>
+<html>
+<head>
+<META HTTP-EQUIV='Refresh' CONTENT='1,URL=update.php'>
+</head>
+<body>
+</body>
+</html
