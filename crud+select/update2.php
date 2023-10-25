31a1fbb08453837bf527182f251cefd6e7e06afd
Petlya2000 <135063891+Petlya2000@users.noreply.github.com>
2023-10-26 02:18:27 +0300

    Create update2.php

---
+++ crud+select/update2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="Content-Type" content="text/html">
+<link rel="stylesheet" type="text/css" href="style.css">
+<title>Update</title>
+</head>
+<body>
+  <form method='post'><br/>
+    <input type="text" name="id" placeholder="Nomer knigi" size="30"><br/>
+    <br/>
+    <input type="text" name="author" placeholder="Author" size="30"><br/>
+    <br/>
+    <input type="text" name="title" placeholder="Title" size="30"><br/>
+    <br/>
+    <input type="text" name="text" placeholder="Text" size="30"><br/>
+    <br/>
+    <input type="submit" id="submit" value="Update"><br/>
+  </form>
+ <?php 
+require_once 'connect.php'; // Подключает файл с логином/паролем и именем БД
+$id = trim($_REQUEST['id']);
+$author = trim($_REQUEST['author']); // Получает содержимое поля "Автор" и убирает возможные пробелы в начале строки
+$title = trim($_REQUEST['title']); // То же самое для поля "Название"
+$text = mysqli_real_escape_string($conn,trim($_REQUEST['text'])); // То же самое для поля "Текст" + (см.ниже)    
+$update_sql = "UPDATE polka1 SET author = '$author', title = '$title', text = '$text' WHERE id = '$id'
+mysqli_query($conn,$update_sql); // отправляем данные в базу
+?>
+  <form method='post' action='allauthor.php'><b/>
+<input id='submitread'  type='submit' value="Вернуться к поиску"><b/><b/>
+</form>
+<form method="post" action="index.html">
+<input id="submitback" type="submit" value="На главную">
+</form>
+</body>
+</html>
