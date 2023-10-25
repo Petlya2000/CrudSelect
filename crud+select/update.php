da112ec54b7aa59641f361ab14652e7e8661bc3c
Petlya2000 <135063891+Petlya2000@users.noreply.github.com>
2023-10-26 01:57:08 +0300

    Create update.php

---
+++ crud+select/update.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="Content-Type" content="text/html">
+<link rel="stylesheet" type="text/css" href="style.css">
+<title>Update</title>
+</head>
+<body>
+  <form action='rec1.php' method='post'><br/>
+    <input type="text" name="id" placeholder="Nomer knigi" size="30"><br/>
+    <br/>
+    <input type="text" name="author" placeholder="Author" size="30"><br/>
+    <br/>
+    <input type="text" name="title" placeholder="Title" size="30"><br/>
+    <br/>
+    <input type="text" name="text" placeholder="Text" size="30"><br/>
+    <br/>
+    <input type="submit" id="submit" value="Update"><br/>
+  </form>
+  <form method='post' action='allauthor.php'><b/>
+<input id='submitread'  type='submit' value="Вернуться к поиску"><b/><b/>
+</form>
+<form method="post" action="index.html">
+<input id="submitback" type="submit" value="На главную">
+</form>
+</body>
+</html>
