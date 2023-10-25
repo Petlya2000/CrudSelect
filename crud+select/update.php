a740c3ed20da977898719e6e043a6b908d204f45
Petlya2000 <135063891+Petlya2000@users.noreply.github.com>
2023-10-25 05:34:55 +0300

    Update update.php

--- crud+select/update.php
+++ crud+select/update.php
@@ -10,24 +10,74 @@
   <?php
 require_once 'connect.php';
 $id = $_REQUEST['nk'];
+$query="select * from polka1 where id='$id'";
+$result = mysqli_query($conn,$query) or die(mysqli_error($conn));
+
 ?>
-<html><body>
-<form method="post">
-<table>
-<?php $books_query=mysql_query("select * from polka1 where id='$id'");
-$rows=mysql_fetch_array($books_query);
-?>
-<tr><td>Author:</td><td><input type="text" name="author" value="<?php echo $rows['author'];  ?>"></td></tr>
-<tr><td>Title:</td><td><input type="text" name="title" value="<?php echo $rows['title'];  ?>"></td></tr>
-<tr><td>Text:</td><td><input type="text" name="Text" value="<?php echo $rows['text'];  ?>"></td></tr>
-<tr><td></td><td><input type="submit" name="submit" value="save"></td></tr>
-</table></form></body></html>
+
+
+<form method="post" action="<?php $_PHP_SELF ?>">
+<table width="400" border="0" cellspacing="1" cellpadding="2">
+<tr>
+
 <?php 
-if (isset($_POST['submit'])){
-$author=$_POST['author'];
-$title=$_POST['title'];
-$text=$_POST['text'];
-mysql_query("UPDATE polka1 SET author='$author', title ='$title', text='$text' where id='$id'");
+
+while($row = mysql_fetch_array($result))
+            {
+
+$id = $row['id'];
+$author = $row['authro'];
+$title = $row['title'];
+$text = $row['text'];
+
+?>
+
+
+<td width="100"></td>
+<td><?=$id?></td>
+</tr>
+<tr>
+<td width="100">Author</td>
+<td><input name="author" type="text" value="<?=$author?>"></td>
+</tr>
+<tr>
+<td width="100">Title</td>
+<td><input name="title" type="text" value="<?=$title?>"></td>
+</tr>
+<tr>
+<td width="100">Text</td>
+<td><input name="text" type="text" value="<?=$text?>"></td>
+</tr>
+<?php } ?>
+<tr>
+<td width="100"> </td>
+<td> </td>
+</tr>
+<tr>
+<td width="100"> </td>
+<td>
+<input name="update" type="submit" id="update" value="Update">
+</td>
+</tr>
+</table>
+</form>
+
+
+
+<?php
+
+if(isset($_POST['update']))
+{
+
+$author =$_POST['author'];
+$title =$_POST['title'];
+$text =$_POST['text'];
+
+
+$sql = mysql_query("UPDATE polka1 SET author = '$author', title = '$title', text = 'text' WHERE id = '$id'");
+
+
+mysqli_query($conn,"UPDATE polka1 SET author='$author', title ='$title', text='$text' where id='$id'");
 header('location:index.php');
 }
 ?>
